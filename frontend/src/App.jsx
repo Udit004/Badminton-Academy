1edd09f08ce5c61d693cfef2d8bd64e3c92778c2
Udit Kumar Tiwari <rajankumart266@gmail.com>
2025-06-11 11:40:12 +0530

    seo improvement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,36 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { AuthProvider } from './context/authContext.jsx'
-import ProtectedRoute from './components/ProtectedRoute'
-import Footer from './components/Footer'
-import Home from './pages/home'
-import About from './pages/about'
-import Contact from './pages/contact'
-import Programs from './pages/programs'
-import Schedule from './pages/schedule'
-import Coaches from './pages/coaches'
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
-import AdminProtectedRoutes from './components/AdminProtectedRoutes';
-import Dashboard from './pages/Dashboard'
-import Profile from './pages/Profile'
+import React from "react";
+import { Helmet } from "react-helmet";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/authContext.jsx";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Footer from "./components/Footer";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Programs from "./pages/programs";
+import Schedule from "./pages/schedule";
+import Coaches from "./pages/coaches";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import AdminProtectedRoutes from "./components/AdminProtectedRoutes";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
 import TermsAndConditions from "./pages/TermsAndConditions";
 import CancellationAndRefund from "./pages/CancellationAndRefund";
 import ShippingAndDelivery from "./pages/ShippingAndDelivery";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
-import BookingPage from './pages/BookingPage.jsx'
-import ConditionalNavbar from './components/ConditionalNavbar.jsx';
+import BookingPage from "./pages/BookingPage.jsx";
+import ConditionalNavbar from "./components/ConditionalNavbar.jsx";
 
 const App = () => {
   return (
     <AuthProvider>
+      <Helmet>
+        <meta
+          name="google-site-verification"
+          content="SGSmDTkUcfGUOsfWJWVBksxsbCZptyQ15tqK1e-SF3M"
+        />
+      </Helmet>
       <Router>
         <div className="flex flex-col min-h-screen bg-gradient-to-br from-gray-900 via-purple-900 to-violet-900">
           <ConditionalNavbar />
@@ -31,46 +38,62 @@ const App = () => {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
-              <Route path="/coaches" element={<ProtectedRoute><Coaches /></ProtectedRoute>} />
+              <Route
+                path="/coaches"
+                element={
+                  <ProtectedRoute>
+                    <Coaches />
+                  </ProtectedRoute>
+                }
+              />
               <Route path="/contact" element={<Contact />} />
-              <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-              <Route path="/cancellation-and-refund" element={<CancellationAndRefund />} />
-              <Route path="/shipping-and-delivery" element={<ShippingAndDelivery />} />
+              <Route
+                path="/terms-and-conditions"
+                element={<TermsAndConditions />}
+              />
+              <Route
+                path="/cancellation-and-refund"
+                element={<CancellationAndRefund />}
+              />
+              <Route
+                path="/shipping-and-delivery"
+                element={<ShippingAndDelivery />}
+              />
               <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-              <Route 
-                path="/programs" 
+              <Route
+                path="/programs"
                 element={
                   <ProtectedRoute>
                     <Programs />
                   </ProtectedRoute>
-                } 
+                }
               />
-              <Route 
-                path="/schedule" 
+              <Route
+                path="/schedule"
                 element={
                   <ProtectedRoute>
                     <Schedule />
                   </ProtectedRoute>
-                } 
+                }
               />
-              <Route 
-                path="/dashboard" 
+              <Route
+                path="/dashboard"
                 element={
                   <ProtectedRoute>
                     <Dashboard />
                   </ProtectedRoute>
                 }
               />
-              <Route 
-                path="/BookingPage" 
+              <Route
+                path="/BookingPage"
                 element={
                   <ProtectedRoute>
                     <BookingPage />
                   </ProtectedRoute>
                 }
               />
-              <Route 
-                path="/Profile" 
+              <Route
+                path="/Profile"
                 element={
                   <ProtectedRoute>
                     <Profile />
@@ -86,7 +109,7 @@ const App = () => {
         </div>
       </Router>
     </AuthProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
